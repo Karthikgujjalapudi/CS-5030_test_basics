cf02036deca7665963749b5ca6dbab926f963199
karthikgujjalapudi@gmail.com <83086333+Karthikgujjalapudi@users.noreply.github.com>
2022-11-08 09:42:43 +0530

    Updated Circle CI, Test-functions and Test-codes

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -1,27 +1,101 @@
+var todoservice = require("./todo.service.js");
+describe("todo test suite", () => {
+  test("truth_value", () => {
+    expect(true).toBe(true);
+  });
 
+  let todo_service = new todoservice();
 
-var todoservice = require('./todo.service.js');
-describe('todo test suite', () => {
+  test("if service instance is created", () => {
+    expect(todo_service instanceof todoservice).toBe(true);
+  });
 
-    test("truth_value", () => {
-        expect(true).toBe(true);
-    });
-    
-    let todo_service = new todoservice();
+  // Initial length of the todo list is 3 // 3 default tasks
+  test("get_todos", () => {
+    expect(todo_service.get_todos().todo.length).toEqual(3);
+  });
 
-    test("if service instance is created", () => {
-        expect(todo_service instanceof todoservice).toBe(true);
-    });
+  test("add_todo", () => {
+    expect(
+      todo_service.add_todo({
+        title: "sample title",
+        describtion: "sample description",
+        done: false,
+      })
+    ).toEqual([
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "sample title",
+        describtion: "sample description",
+        done: false,
+      },
+    ]);
+  });
 
-    
-    // Initial length of the todo list is 3 // 3 default tasks
-    test("get_todos", () => {
-        expect(todo_service.get_todos().todo.length).toEqual(3);
-    });
+  test("delete_todo", () => {
+    expect(todo_service.delete_todo(0)).toEqual([
+      {},
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "sample title",
+        describtion: "sample description",
+        done: false,
+      },
+    ]);
+  });
 
-    
-
-    // Write all your test cases here that corresponds to software requirements
-
+  test("update_todo", () => {
+    expect(
+      todo_service.update_todo(0, {
+        title: "sample title 2",
+        describtion: "sample description 2",
+        done: false,
+      })
+    ).toEqual([
+      {
+        title: "sample title 2",
+        describtion: "sample description 2",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "sample title",
+        describtion: "sample description",
+        done: false,
+      },
+    ]);
+  });
 
 });
